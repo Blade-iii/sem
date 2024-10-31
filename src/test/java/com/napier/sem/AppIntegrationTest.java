6ae379db4e25615b8acdbaf47397fe91804c5ed8
Blade-iii <54221403+Blade-iii@users.noreply.github.com>
2024-10-31 17:13:58 +0000

    testGetEmployeeManager Created

--- src/test/java/com/napier/sem/AppIntegrationTest.java
+++ src/test/java/com/napier/sem/AppIntegrationTest.java
@@ -41,7 +41,10 @@ public class AppIntegrationTest {
     @Test
     void testGetEmployeeManager() {
         ArrayList<Employee> manager = app.getEmployeeManager("Masako","Angiulli");
-        assertEquals(manager.size(), 1);
+        assertEquals(manager.size(), 2);
         assertNotNull(manager);
+        assertEquals(manager.get(0).first_name, "Masako");
+        assertEquals(manager.get(0).last_name, "Angiulli");
+        assertNotEquals(manager.isEmpty(), true);
     }
 }
